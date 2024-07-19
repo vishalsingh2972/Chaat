eeb6defca81b1de7301a65a0140694be3b44222e
Vishal Singh <vishalsinghpanwar02@gmail.com>
2024-07-19 22:22:28 +0530

    chat component start

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,7 +1,10 @@
 export const Chat = () => {
   return (
     <div className="chat-app">
-      Chat
+      <form className="new-message-form">
+        <input className="new-message-input" placeholder="Type your message here..."/>
+        <button type="submit" className="send-button"> Send 🥂</button>
+      </form>
     </div>
   )
 }
